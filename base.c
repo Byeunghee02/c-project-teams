4997109d7479fc1296f63ebceb8a0bd5c35b7cfc
Byeunghee02 <choibh6258@hanyang.ac.kr>
2021-11-19 01:34:30 +0900

    plow up
    갈아 엎음

--- base.c
+++ base.c
@@ -2,71 +2,69 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(){
+int main()
+{
     FILE *fin = fopen("input.txt", "r");
 
-    //입력 개수
+    //input count
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t readItem;
     int readLine = 0;
-    int a, b, readItem;
-    readItem = fscanf(fin,"%d %d",&a, &b);
-    while(readItem >= 0){
+    int val1, val2;
+    while ((readItem = getline(&line,&len,fin)) != EOF)
+    {
         readLine++;
-        readItem = fscanf(fin,"%d %d",&a, &b);
-    } 
-    srand(time(NULL));
-    
-    //input list
-    int ** inputList = (int**) malloc(sizeof(int*)*readLine);
-    for (int i = 0; i < readLine; i++)
-    {
-        inputList[i] = (int*) malloc(sizeof(int)*2);
     }
-    
-    //matrix list
-    int *** matrixList =(int***) malloc(sizeof(int**)*readLine);
-    
-    //input matrix
+    printf("%d\n", readLine);
+
+
+    //input list
+    int **inputList = (int **)malloc(sizeof(int *) * readLine);
     for (int i = 0; i < readLine; i++)
     {
-        int a, b;
-        fscanf(fin,"%d %d",&a,&b);
-        inputList[i][0] = a; inputList[i][1] = b;
-        matrixList[i] = (int**) malloc(sizeof(int*)*b);
-        for(int j = 0; j < b; j++){
-            matrixList[i][j] = (int*) malloc(sizeof(int)*a);
-        }
-        for(int j = 0; j < b; j++){
-            for(int k = 0; k < a; k++){
-                matrixList[i][j][k] = rand() % 10 + 1;
-                printf("%d~ ",matrixList[i][j][k]);
-            }
-        }
+        inputList[i] = (int *)malloc(sizeof(int) * 2);
+    }
+    for (int i = 0; i < readLine; i++)
+    {
+        fscanf(fin, "%d %d", &inputList[i][0], &inputList[i][1]);
+        printf("%d %d ", inputList[i][0], inputList[i][1]);
     }
     fclose(fin);
-    FILE* fout = fopen("output.txt", "w+");
+
+    //matrix generation
+    int ***matrixList = (int ***)malloc(sizeof(int **) * readLine);
+    srand(time(NULL));
     for (int i = 0; i < readLine; i++)
     {
-        fprintf(fout,"%d matrix\n", i+1);
-        for (int j = 0; j < inputList[i][1]; j++)
+        val1 = inputList[i][0], val2 = inputList[i][1];
+        matrixList[i] = (int **)malloc(sizeof(int *) * val1);
+        for (int j = 0; j < val1; j++)
         {
-            for (int k = 0; k < inputList[i][0]; k++)
+            // printf ("%d %d", val1, val2);
+            matrixList[i][j] = (int *)malloc(sizeof(int) * val2);
+            for (int k = 0; k < val2; k++)
             {
-                fprintf(fout,"%d ", matrixList[i][j][k]);
+                matrixList[i][j][k] = rand() % 10 + 1;
+                // printf("%d,",matrixList[i][j][k]);
             }
-            fprintf(fout,"\n");
         }
-        
     }
-    fclose(fout);
-    printf("done");
-    printf("%d",readLine);
-    // for (int i = 0; i < readLine; i++)
-    // {
-    //     for (int j = 0; j < 2; j++)
-    //     {
-    //         printf("%d ", inputList[i][j]);
-    //     }
-        
-    // }
+
+    FILE *fout = fopen("output.txt", "w+");
+    for (int i = 0; i < readLine; i++)
+    {
+        fprintf(fout, "%d matrix", i+1);
+        for (int j = 0; j < inputList[i][0]; j++)
+        {
+            for (int k = 0; k < inputList[i][1]; k++)
+            {
+                fprintf(fout, "%d " , matrixList[i][j][k]);
+            }
+            fprintf(fout, "\n");
+        }
+        fprintf(fout, "\n");
+    }
     
+    fclose(fout);
 }
